b17e26deb6b126f6836a055e9d062e313c037de0
Alla Umesh <23MH1A05E7@acoe.edu.in>
2025-03-08 19:56:35 +0530

    Time: 19 ms (54.34%), Space: 59.8 MB (5.75%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,15 +1,26 @@
+import java.util.*;
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // HashMap to store the most recent index of each number in the array
+        // The key is the number, and the value is its most recent index
+        HashMap<Integer, Long> map = new HashMap<>();
+        
+        // Iterate through the array with a long index 'i'
         for (long i = 0; i < nums.length; i++) {
-            if (i > 5000) {
-                return false;
+            
+            // Check if the current number is already in the HashMap and if the difference
+            // between the current index 'i' and the previous index of the same number is less than or equal to k
+            if (map.containsKey(nums[(int)i]) && Math.abs(i - map.get(nums[(int)i])) <= k) {
+                // If the condition is true, return true as we found a nearby duplicate
+                return true;
             }
-            for (long j = 0; j < i; j++) {
-                if (nums[(int) i] == nums[(int) j] && Math.abs(i - j) <= k) {
-                    return true;
-                }
-            }
+            
+            // If no duplicate is found within the range 'k', update the map with the current number and its index
+            map.put(nums[(int)i], i);
         }
+        
+        // If the loop finishes without finding any nearby duplicate, return false
         return false;
     }
 }
