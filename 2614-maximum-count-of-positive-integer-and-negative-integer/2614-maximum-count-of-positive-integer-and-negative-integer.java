91b5b5c9f2da9e510144a0affa518085dde7b855
Alla Umesh <23MH1A05E7@acoe.edu.in>
2025-03-12 21:43:13 +0530

    Time: 0 ms (100%), Space: 45.4 MB (15.76%) - LeetHub

---
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumCount(int[] nums) {
+        int c1 = 0;
+        int c2 = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                c1++;
+            } else if (nums[i] > 0) {
+                c2++;
+            }
+        }
+        
+        return Math.max(c1, c2);
+    }
+}
