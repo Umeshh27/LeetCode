e24af18570d5128d9160506156b06afdf071d280
Alla Umesh <23MH1A05E7@acoe.edu.in>
2025-03-28 14:45:32 +0530

    Time: 0 ms (100%), Space: 43.1 MB (79.13%) - LeetHub

---
+++ 1975-minimum-distance-to-the-target-element/1975-minimum-distance-to-the-target-element.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int minDist = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == target) {
+                minDist = Math.min(minDist, Math.abs(i - start));
+            }
+        }
+        return minDist;
+    }
+}
