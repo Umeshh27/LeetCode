8fbe926615341db09384b9031b59d427416a35be
Alla Umesh <23MH1A05E7@acoe.edu.in>
2025-06-15 19:02:54 +0530

    Time: 1 ms (23.84%), Space: 41.1 MB (24.48%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) return false;
+        for (int i = 0; i < 31; i++) {
+            if (n == Math.pow(4, i)) return true;
+        }
+        return false; 
+    }
+}
