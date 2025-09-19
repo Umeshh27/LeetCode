9b9e49ff31a0611a28c25f9ec19b29500beb229b
Alla Umesh <23MH1A05E7@acoe.edu.in>
2025-09-19 22:34:34 +0530

    Time: 0 ms (100%), Space: 40.3 MB (67.95%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int left = 0, right = num;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                left = (int) mid + 1;
+            } else {
+                right = (int) mid - 1;
+            }
+        }
+        return false;
+    }
+}
