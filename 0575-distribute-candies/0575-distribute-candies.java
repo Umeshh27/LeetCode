d139dfb8feca4cef1ca6d0a6fc169b613b1d8668
Alla Umesh <23MH1A05E7@acoe.edu.in>
2025-10-08 17:58:55 +0530

    Time: 31 ms (51.4%), Space: 45.6 MB (77.31%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -1,11 +1,11 @@
-import java.util.*;
-
 class Solution {
     public int distributeCandies(int[] candyType) {
-        Set<Integer> set = new HashSet<>();
-        for (int candy : candyType) {
-            set.add(candy);
+        int n = candyType.length / 2;
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int arr : candyType) {
+            set.add(arr);
         }
-        return Math.min(set.size(), candyType.length / 2);
+        return Math.min(n, set.size());
     }
 }
