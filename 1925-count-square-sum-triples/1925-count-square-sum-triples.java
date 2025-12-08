4bc19ab4420cbb6d80dbece17385be0fbe26ecaa
Alla Umesh <23MH1A05E7@acoe.edu.in>
2025-12-08 22:47:08 +0530

    Time: 86 ms (18.79%), Space: 42.1 MB (49.69%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countTriples(int n) {
+        int count =0;
+        for(int c=1;c<=n;c++){
+            int sq=c*c;
+        
+
+        for(int a=1;a<c ; a++){
+            for(int b=1;b<c;b++){
+                if(a*a+b*b==sq){
+                    count++;
+                }
+            }
+        }
+        }
+        return count;
+    }
+}
