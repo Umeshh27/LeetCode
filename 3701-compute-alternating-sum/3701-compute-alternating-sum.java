ffcad07ad40933f8a8b1788afae9067e9c52bccd
Alla Umesh <umeshalla73@gmail.com>
2026-07-16 20:58:59 +0530

    Time: 1 ms (98.73%), Space: 46.8 MB (10.05%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int even=0;
+        int odd=0;
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0){
+                even=even+nums[i];
+            }
+            else odd=odd+nums[i];
+        }
+        return even-odd;
+    }
+}
