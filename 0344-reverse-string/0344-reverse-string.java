5b9d62df9934a5ddc76e2eea0bfb633f448d6509
Alla Umesh <umeshalla73@gmail.com>
2026-08-06 11:23:30 +0530

    Time: 2 ms (7.46%), Space: 48.1 MB (75.11%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public void reverseString(char[] s) {
+        StringBuilder sb=new StringBuilder(new String(s)).reverse();
+        for(int i=0;i<s.length;i++){
+            s[i]=sb.charAt(i);
+        }
+    }
+}
